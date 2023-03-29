3b95442d5af8c1ecf15f91e53039f7ef67e4a236
AUTUMN <krshlee22@naver.com>
2023-03-30 02:36:53 +0900

    socket.to, socket.emit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,11 +8,29 @@ room.hidden = true;
 
 let roomName;
 
+function addMessage(message) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("input");
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${input.value}`);
+        input.value = "";
+    });
+}
+
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
+    const form = room.querySelector("form");
+    form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -24,3 +42,13 @@ function handleRoomSubmit(event) {
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+    addMessage("someone joined!");  
+})
+
+socket.on("bye", () => {
+    addMessage("someone left ㅠㅠ");
+})
+
+socket.on("new_message", (msg) => addMessage(msg));
